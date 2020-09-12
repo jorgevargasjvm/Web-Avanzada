4da99f0a170be1d638f679e31a8b93ee2a590d9e
Jorge Vargas <jorgevargasmateo@gmail.com>
2020-09-12 13:40:57 -0400

    Creando proyecto crudweb

---
+++ crudwa/src/main/java/com/example/crudwa/CrudwaApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudwaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudwaApplication.class, args);
+    }
+
+}
---
+++ crudwa/src/test/java/com/example/crudwa/CrudwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudwaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
